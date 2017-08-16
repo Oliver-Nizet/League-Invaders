b447ba8cca8ff3df8aa3cf33a5a026fec11162ce
Oliver-Nizet <league@WTS22.attlocal.net>
2017-08-15 18:20:49 -0700

    hello

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,19 +4,31 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer timer;
-	GameObject go;
+	final int MENU_STATE = 0;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	int currentState = MENU_STATE;
+
 	GamePanel() {
-		go = new GameObject();
 		timer = new Timer(1000 / 60, this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		go.update();
+		repaint();
+		if (currentState == MENU_STATE) {
+			updateMenuState();
+		} else if (currentState == GAME_STATE) {
+			updateGameState();
+		} else if (currentState == END_STATE) {
+			updateEndState();
+		}
+
 	}
 
 	void startGame() {
@@ -24,7 +36,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	}
 
 	public void paintComponent(Graphics g) {
-		go.draw(g);
+		if (currentState == MENU_STATE) {
+			drawMenuState(g);
+		} else if (currentState == GAME_STATE) {
+			drawGameState(g);
+		} else if (currentState == END_STATE) {
+			drawEndState(g);
+		}
 	}
 
 	@Override
@@ -36,6 +54,12 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		System.out.println("o");
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			currentState++;
+		}
+		if (currentState > END_STATE) {
+			currentState = MENU_STATE;
+		}
 	}
 
 	@Override
@@ -44,4 +68,29 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 	}
 
+	void updateMenuState() {
+
+	}
+
+	void updateGameState() {
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(LeagueInvaders.ORANGEBRO);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+
+	}
+
+	void drawGameState(Graphics g) {
+
+	}
+
+	void drawEndState(Graphics g) {
+
+	}
 }
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,8 +1,11 @@
-import javax.swing.*;
+import java.awt.Color;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-	final int width = 500;
-	final int height = 800;
+	static final int WIDTH = 500;
+	static final int HEIGHT = 800;
+	static final Color ORANGEBRO = new Color(240, 100, 30);
 	JFrame frame;
 	GamePanel gp;
 
@@ -13,16 +16,15 @@ public class LeagueInvaders {
 	LeagueInvaders() {
 		frame = new JFrame();
 		gp = new GamePanel();
+		frame.addKeyListener(gp);
 		setup();
 	}
 
 	void setup() {
 		gp.startGame();
-
 		frame.add(gp);
-		frame.addKeyListener(gp);
 		frame.setVisible(true);
-		frame.setSize(width, height);
+		frame.setSize(WIDTH, HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
